8f30fe4c08c0cbcc2c0435473df90070e327dfe1
Cristina Iannone <iannonecristina@gmail.com>
2017-04-03 22:39:40 +0100

    Add update to fizzbuzz_spec.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,34 @@
-require '../lib/fizzbuzz'
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
-end
 
-describe 'fizzbuzz' do
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
-end
+    context "when passed 3" do
+      it 'returns "fizz"' do
+        expect(fizzbuzz(3)).to eq 'fizz'
+      end
+    end
 
-describe 'fizzbuzz' do
-  it 'retuns "fizzbuzz" when passed a multiple of 3 and 5' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
-end
+    context "when passed 5" do
+     it 'returns "buzz"' do
+       expect(fizzbuzz(5)).to eq 'buzz'
+     end
+    end
 
-describe 'fizzbuzz' do
-  it 'returns the number if the number passed is not a multiple of 3 or 5' do
-    expect(fizzbuzz(2)).to eq 2
+    context "when passed a multiple of 3 and 5" do
+     it 'retuns "fizzbuzz"' do
+       expect(fizzbuzz(15)).to eq 'fizzbuzz'
+     end
+    end
+
+    context "when it is not passed a multiple of 3 or 5" do
+     it 'returns the number' do
+       expect(fizzbuzz(2)).to eq 2
+     end
+    end
+
+    context "when it is passed 0" do
+      it 'returns 0' do
+        expect(fizzbuzz(0)).to eq 0
+      end
+    end
   end
-end
